2b1141bc5c96d77e02ca9665d28dbf3f8f951c7f
crypto-bender <dac3mf@virginia.edu>
2022-07-07 13:10:20 -1000

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./client/src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'client'),
+    },
+    compress: true,
+    port: 3000
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
