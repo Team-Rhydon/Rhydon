5b2bab9cee6675e15825d325dde9f7d0c4f81668
Daniel Chu <sharqueendc@gmail.com>
2022-07-07 15:01:22 -1000

    Delete Client/src directory

--- Client/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {createRoot} from "react-dom/client";
-import App from "./App.jsx";
-const container = document.createElement("div");
-container.setAttribute("id", "root");
-document.body.appendChild(container);
-const root = createRoot(container);
-root.render(<App />);
