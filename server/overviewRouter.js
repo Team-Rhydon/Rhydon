96e8e69015344bbaf19fdbed00f1bb70681bd93a
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-07-08 11:58:40 -0700

    pushing to overview branch

---
+++ server/overviewRouter.js
@@ -0,0 +1,10 @@
+var overviewRouter = require('express').Router();
+
+// connect controller methods to corresponding routes
+
+
+//product route
+
+overviewRouter.get('/products',  /* controller func*/)
+
+module.exports = overviewRouter
