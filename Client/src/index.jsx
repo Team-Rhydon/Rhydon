4f1f3e3446e172b136281396e1415a169e50b0f9
crypto-bender <dac3mf@virginia.edu>
2022-07-07 14:36:09 -1000

    app now correctly renders. should be good to begin implementing widgets

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {createRoot} from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
-const root = document.createElement("div");
-root.setAttribute("id", "root");
-document.body.appendChild(root);
-const rootApp = createRoot(root);
-rootApp.render(<App />);
+const container = document.createElement("div");
+container.setAttribute("id", "root");
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(<App />);
