9e03ced5a7ed03b9b224bfdd75a1127970c4226f
crypto-bender <dac3mf@virginia.edu>
2022-07-15 00:38:09 -1000

    test commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import React, {Component} from "react";
 import Related from './Related/Related.jsx';
-<<<<<<< HEAD
 import RatingsWidget from './Ratings/RatingsWidget.jsx';
 const axios = require("axios");
-=======
 import Overview from './overview/Overview.jsx'
->>>>>>> main
 
 class App extends Component{
   constructor(props){
@@ -26,7 +23,6 @@ class App extends Component{
     return(
       <div className="App">
         <h1> Rhydon Store </h1>
-<<<<<<< HEAD
         <div style={{border: "1px solid black", height: "33em"}}>
           Product Overview Component
           <h2>Product Overview Component</h2>
@@ -37,9 +33,6 @@ class App extends Component{
         <div>
           <RatingsWidget product={this.state.product}/>
         </div>
-=======
-        <Overview />
->>>>>>> main
       </div>
     );
   }
