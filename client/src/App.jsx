6f2c5a102e492f67969a9a41b2635186da9e1254
markmiw <markmiw6@gmail.com>
2022-07-19 12:44:37 -1000

    updated styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -134,10 +134,12 @@ function App() {
   return (
     <div data-testid="" className="app">
       <Nav updateCurrentProduct={updateCurrentProduct}/>
+      <div className='widgets'>
       <Overview className="overview-widget" {...product}/>
       <Related key='related' product={product} updateCurrentProduct={updateCurrentProduct} hidePreview={hidePreview}/>,
       <Outfit key='outfit' product={product} outfits={outfits} removeOutfit={removeOutfit} addToOutfit={addToOutfit} carouselPos={carouselPos}/>
       <RatingsWidget details={product.details} meta={product.reviews} ref={ratingsRef}/>
+      </div>
     </div>
   );
 }
