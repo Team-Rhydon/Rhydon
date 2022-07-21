8a2e7907c07f0ffd63c198b3100827be157fe7e1
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-07-21 10:19:53 -0700

    added fb and twitter share buttons

--- client/src/overview/components/Share.jsx
+++ client/src/overview/components/Share.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import {FaFacebookF, FaTwitter, FaPinterestP} from 'react-icons/fa';
+import { FacebookShareButton, TwitterShareButton, PinterestShareButton  } from 'react-share';
+// import 'share-button-links-react/main.css'
 
 const Share = (props) => {
   return (
     <div className="share-icons">
-      <button className="facebook"><FaFacebookF /></button>
-      <button className="twitter"><FaTwitter /></button>
-      <button className="pinterest"><FaPinterestP /></button>
+      <FacebookShareButton
+        url='https://developers.facebook.com/docs/plugins/'
+        id="facebook"><FaFacebookF />
+      </FacebookShareButton>
+      <TwitterShareButton
+        url='https://twitter.com/intent/tweet'
+        id="twitter"><FaTwitter />
+      </TwitterShareButton>
+      <button
+        url='//assets.pinterest.com/js/pinit.js'
+        id="pinterest"><FaPinterestP />
+      </button>
     </div>
+
   );
 };
 
